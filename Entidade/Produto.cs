cd7cc6387d6b51fe1ae0cc984ea2bf4ea3b8f16e
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-01-16 18:16:24 -0300

    Adicionar arquivos de projeto.

---
+++ Entidade/Produto.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+
+namespace ConsolePOOProduto.Entidade
+{
+    internal class Produto
+    {
+        public string Nome = "";
+        public double Preco;
+        public int Quantidade;
+
+        public Produto(string nome, double preco, int quaitidade)
+        {
+            this.Nome = nome;
+            this.Preco = preco;
+            this.Quantidade = quaitidade;
+        }
+        public Produto(string nome, double preco) {
+            this.Nome = nome;
+            this.Preco = preco;
+            this.Quantidade = 0;// Ou não informar nada
+        }
+
+        public Produto()
+        {
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return this.Quantidade * this.Preco;
+        }
+
+        public void AdicionarProdutos(int quantity)
+        {
+            this.Quantidade += quantity;
+        }
+
+        public void RemoverProdutos(int quantity)
+        {
+            this.Quantidade -= quantity;
+        }
+
+        public override string ToString()
+        {
+            return this.Nome + ", $" +
+                   this.Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " +
+                   this.Quantidade + " unidades, Total: " +
+                   this.ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsolePOOProduto.Entidade;
+using System.Globalization;
+
+Console.Write("Entre com  os dados do produto: ");
+Console.Write("Nome:");
+string nome = Console.ReadLine();
+
+Console.Write("Preço: ");
+double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Quantidade no estoque: ");
+int quantidade = int.Parse(Console.ReadLine());
+
+Produto produto = new Produto(nome, preco); // Ou Produto produto = new Produto(nome, preco, quantidade);
+                                            // Ou Produto produto = new Produto() { Nome = "TV", Preco = 9000.00, Quantidade = 5 };
+
+/// Esse é o  exemplo fazendo o override no método virtual ToString();
+Console.WriteLine("Dados do produto: " + produto);
+Console.Write("Digite o numero de produtos a ser adicionado no estoque: ");
+produto.AdicionarProdutos(int.Parse(Console.ReadLine()));
+Console.Write("Digite  o Numero de produtos a ser removido do estoque ");
+produto.RemoverProdutos(int.Parse(Console.ReadLine()));
+Console.WriteLine("Dados do produto: " + produto);
